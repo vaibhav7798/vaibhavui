a52a13d9929fc91c74252609ff01848e623c2ff9
vaibhav Name <you@example.com>
2023-03-23 13:51:24 +0530

    number methods concept

---
+++ numberMethods.js
@@ -0,0 +1,11 @@
+var data=100;
+var res=data.toString();//convert number to string
+console.log(res);
+
+var dec=1.2324;
+var res=dec.toFixed(2);
+console.log(res);
+
+var num="555";
+var res=parseInt(num);//Converts a string to an integer.
+console.log(res);//555

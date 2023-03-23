3c10af6f9750da1a842af14877bec9f98c5a28dc
vaibhav Name <you@example.com>
2023-03-23 13:59:31 +0530

    dom concept

---
+++ dom.js
@@ -0,0 +1,8 @@
+function colorchange()
+{
+   var element=document.getElementById("head");
+   console.log(element.innerHTML); 
+   element.style.backgroundColor="green";
+
+   //element.innerHTML="hi vaibhav kabade..";
+ }

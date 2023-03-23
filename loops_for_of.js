f5424d9fc7a7dcb3a60abf52d4b706f6113e2f0b
vaibhav Name <you@example.com>
2023-03-23 13:50:42 +0530

    loops for of concept

---
+++ loops_for_of.js
@@ -0,0 +1,28 @@
+var city=["jalgaon","nashik","pune","mumbai"];
+
+var newList=[];
+for(var i of city)
+{
+// console.log(i);
+newList.push(i);
+}
+
+console.log(i);
+console.log("newlist is.."+newList);
+
+
+
+var number=20;
+
+while(number>10)
+{
+ number--;//if we are not giving increment OR decrement operator then Compiler will go in INFINITE LOOP.
+console.log(number);
+}
+
+var number1=6;
+
+do{
+    number1 --;//if we are not giving increment OR decrement operator then Compiler will go in INFINITE LOOP.
+    console.log(number1);
+}while(number1>1)
